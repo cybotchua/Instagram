016b3ee30bb5ffa96a93879c846e4172b6ef545e
Unknown <lihheng90@hotmail.com>
2018-02-19 17:25:40 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Instagram/HomeCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeCell.swift
+//  Instagram
+//
+//  Created by Lih Heng Yew on 19/02/2018.
+//  Copyright © 2018 Terence Chua. All rights reserved.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
