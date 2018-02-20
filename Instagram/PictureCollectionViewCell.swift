533e5b6aa6e8638d886b9ef85fa17833a1198b04
cybotchua <cybotchua@hotmail.com>
2018-02-20 12:02:32 +0800

    fb cancel button fixed, collectionView added with image

---
+++ Instagram/PictureCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PictureCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Terence Chua on 20/02/2018.
+//  Copyright © 2018 Terence Chua. All rights reserved.
+//
+
+import UIKit
+
+class PictureCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
