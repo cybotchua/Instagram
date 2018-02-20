21a8263cae2fd211081d0e825208d94881f07baf
Unknown <lihheng90@hotmail.com>
2018-02-20 17:33:41 +0800

    Auto stash before merge of "master" and "origin/master"
    added observe image

--- Instagram/Image.swift
+++ Instagram/Image.swift
@@ -13,12 +13,20 @@ class Image {
     var comments : String = ""
     var uploaderUID : String = ""
     var imageURL : String = ""
-    
+    var timeStamp : NSNumber = 0
+    var currentTimeStamp = NSDate()
+
     init(imageUID: String, imageDict: [String : Any]) {
         self.imageUID = imageUID
         self.comments = imageDict["comments"] as? String ?? "No comments"
         self.uploaderUID = imageDict["uploaderUID"] as? String ?? "No uploaderUID"
         self.imageURL = imageDict["imageURL"] as? String ?? "No URL"
+        timeStamp = imageDict["timeStamp"] as? NSNumber ?? 0
+
+        let seconds = timeStamp.doubleValue
+        let timestampDate = NSDate(timeIntervalSince1970: seconds)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss a"
     }
     
 }
