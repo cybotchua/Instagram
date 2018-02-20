1a9d1702ee064d0462f7940d05b0a08650a72dd3
Unknown <lihheng90@hotmail.com>
2018-02-20 10:35:12 +0800

    Auto stash before merge of "master" and "origin/master"
    Created XIB

---
+++ Instagram/HomeTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTableViewCell.swift
+//  Instagram
+//
+//  Created by Lih Heng Yew on 20/02/2018.
+//  Copyright © 2018 Terence Chua. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var homeImageView: UIImageView!
+    
+    
+}
--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -19,30 +19,31 @@ class HomeViewController: UIViewController {
         } catch {
             
         }
-     
-
     }
-        
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension HomeViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("HomeTableViewCell", owner: self, options: nil)?.first as! HomeTableViewCell
+
+        return cell
+    }
+}
+
+
